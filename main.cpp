42f4fd0d922b1f8dc3fb7375316992e95746925f
AnxiYetti <anxiyetti@gmail.com>
2026-08-19 14:18:06 -0400

    Refactor combat narration and gauntlet reporting

--- main.cpp
+++ main.cpp
@@ -4,53 +4,59 @@
 #include "mech.h"
 #include "weapon.h"
 
+// Function to generate random numbers for use with dealDamage
 int randomDmgNumber(std::default_random_engine& gen, Weapon& weapon) {
-    std::uniform_int_distribution<int> distribution(weapon.getMinDamage(), weapon.getMaxDamage()); // needs min and max values of weapon
+    std::uniform_int_distribution<int> distribution(weapon.getMinDamage(), weapon.getMaxDamage());
     int rngDamage = distribution(gen);
     return rngDamage;
 }
 
+// Controls the combat loop for gauntlet runs
 void combat(Mech& mech1, Mech mech2, Weapon& weapon1, Weapon& weapon2, std::default_random_engine& gen) {
-    int i = 0;
-    bool activeCombat = true;
+    int rounds = 0;
+    // Run combat until mech 2 is destroyed.
     while (!mech2.isDestroyed()) {
-        std::cout << "Round " << i+1 << "!" << '\n'
-                  << "Inexorable swings beamsword at the Bum!" << '\n';
+        std::cout << "Round " << rounds+1 << "!" << '\n'
+                  << mech1.getName() << " swings " << weapon1.getName() << " at " << mech2.getName() << "!" <<  std::endl;
         int mech1rngDmg = randomDmgNumber(gen, weapon1);
-        std::cout << "Inexorable dealt " << mech1rngDmg << " damage!" << std::endl; 
+        std::cout << mech1.getName() << " dealt " << mech1rngDmg << " damage to " << mech2.getName() + "." << std::endl; 
         mech1.dealDamage(mech2, mech1rngDmg);
-        std::cout << "Bum's health: " << mech2.getHealth() << '\n';
+        std::cout << mech2.getName() + "'s" << " health: " << mech2.getHealth() << std::endl;
         if (mech2.isDestroyed()) {
-            std::cout << "Bum has been destroyed!" << std::endl;
+            std::cout << mech2.getName() << " has been destroyed!" << std::endl;
             break;
         }
-        std::cout << "Bum responds with a Beamrifle!" << '\n';
+        std::cout << mech2.getName() << " responds with a " << weapon2.getName() + "!" << std::endl;
         int mech2RngDmg = randomDmgNumber(gen, weapon2);
-        std::cout << "Bum dealt " << mech2RngDmg << " damage!" << std::endl;
+        std::cout << mech2.getName() << " dealt " << mech2RngDmg << " damage!" << std::endl;
         mech2.dealDamage(mech1, mech2RngDmg); // Needs nothing now! lol
-        std::cout << "Inexorable's health: " << mech1.getHealth() << '\n';
+        std::cout << mech1.getName() + "'s" << " health: " << mech1.getHealth() << std::endl;
         if (mech1.isDestroyed()) { 
-            std::cout << "Inexorable has been destroyed!" << std::endl;
+            std::cout << mech1.getName() << " has been destroyed!" << std::endl;
             break;
         }
-        i++;
+        rounds++;
     }
 }
 
 int main() {
     Mech Inex(150, "Inexorable");
-    Mech Bum(50, "Bum");
-    Weapon Bs("Beamsword", 1, 25);
+    Mech Scrap(50, "Scrapper");
+    Weapon Bs("Beamsword", 10, 25);
     Weapon Br("Beamrifle", 1, 10);
     std::random_device rd;
     std::default_random_engine generator(rd());
     int gauntlet_length = 0;
     while (!Inex.isDestroyed()) {
-    std::cout << std::string(20, '*') << std::endl;
-    combat(Inex, Bum, Bs, Br, generator);
+        std::cout << std::string(20, '*') << std::endl;
+        combat(Inex, Scrap, Bs, Br, generator);
+    if (Inex.isDestroyed()) {
+        break;
+    }
     gauntlet_length++;
     }
-    std::cout << "Inexorable.... Has been destroyed..." << std::endl;
-    std::cout << "He stood valiantly against " << gauntlet_length << " enemies." << std::endl;
+    std::cout << Inex.getName() << ".... Has been destroyed..." << std::endl;
+    std::cout << "He left " << gauntlet_length << " enemies in his wake." << std::endl;
+    std::cout << "He fell valiantly against the " << gauntlet_length+1 << "th enemy." << std::endl;
     return 0;
 }
